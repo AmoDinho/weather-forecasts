6cc1c06667a1b267f96d15b64f4fe7176c92fdb3
Amo Moloko <u14284783@tuks.co.za>
2018-08-13 11:47:02 +0200

    CityPicker
    The city picker is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
+import CityPicker from "./components/CityPicker";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectValue: "Cape Town"
+    };
+
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  handleSelectChange(value) {
+    this.setState({ selectValue: value });
+  }
+
   render() {
+    const { selectValue } = this.state;
     return (
       <div className="App">
         <div className="App_Header">
           <h1>Weather App</h1>
+        </div>
+
+        <div>
+          <CityPicker
+            value={selectValue}
+            onSelectChange={this.handleSelectChange}
+          >
+            Choose a city
+          </CityPicker>
         </div>
       </div>
     );
