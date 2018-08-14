ec693e7e1d38ecec05701e10f6d718c1643a5eb2
Amo Moloko <u14284783@tuks.co.za>
2018-08-14 13:47:19 +0200

    Data
    added a data structure

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -10,23 +10,21 @@ import {
   Button
 } from "reactstrap";
 
-const WeatherCard = props => {
-  return (
-    <div>
-      <Card>
-        <CardImg
-          top
-          width="100%"
-          src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>Day</CardTitle>
-          <CardSubtitle>23c</CardSubtitle>
-        </CardBody>
-      </Card>
-    </div>
-  );
-};
+const WeatherCard = ({ Day, Tempreture }) => (
+  <div>
+    <Card>
+      <CardImg
+        top
+        width="100%"
+        src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+        alt="Card image cap"
+      />
+      <CardBody>
+        <CardTitle>{Day}</CardTitle>
+        <CardSubtitle>{Tempreture}</CardSubtitle>
+      </CardBody>
+    </Card>
+  </div>
+);
 
 export default WeatherCard;
--- src/containers/Forecast.js
+++ src/containers/Forecast.js
@@ -3,7 +3,105 @@ import ReactDOM from "react-dom";
 import { Container, Row, Col, Jumbotron } from "reactstrap";
 import WeatherCard from "../components/WeatherCard";
 
-class Forecast extends Component {
+const weather = [
+  {
+    "Cape Town": {
+      Days: [
+        {
+          ObjectId: 1,
+          Day: "Monday",
+          tempH: 23,
+          tempL: 12,
+          sunrise: "7:25",
+          sunset: "7:25",
+          windSpeed: "14 km/h",
+          humidity: "24%",
+          pressure: "1026.00 mb",
+          uvIndex: 4,
+          cloudCover: "40%",
+          ceiling: "9144m",
+          dewPoint: -6,
+          visibility: 16
+        },
+        {
+          ObjectId: 2,
+          Day: "Tuesday",
+          tempH: 23,
+          tempL: 12,
+          sunrise: "7:25",
+          sunset: "7:25",
+          windSpeed: "14 km/h",
+          humidity: "24%",
+          pressure: "1026.00 mb",
+          uvIndex: 4,
+          cloudCover: "40%",
+          ceiling: "9144m",
+          dewPoint: -6,
+          visibility: 16
+        },
+
+        {
+          ObjectId: 3,
+          Day: "Wednesday",
+          tempH: 23,
+          tempL: 12,
+          sunrise: "7:25",
+          sunset: "7:25",
+          windSpeed: "14 km/h",
+          humidity: "24%",
+          pressure: "1026.00 mb",
+          uvIndex: 4,
+          cloudCover: "40%",
+          ceiling: "9144m",
+          dewPoint: -6,
+          visibility: 16
+        },
+        {
+          ObjectId: 4,
+          Day: "Thursday",
+          tempH: 23,
+          tempL: 12,
+          sunrise: "7:25",
+          sunset: "7:25",
+          windSpeed: "14 km/h",
+          humidity: "24%",
+          pressure: "1026.00 mb",
+          uvIndex: 4,
+          cloudCover: "40%",
+          ceiling: "9144m",
+          dewPoint: -6,
+          visibility: 16
+        },
+        {
+          ObjectId: 5,
+          Day: "Friday",
+          tempH: 23,
+          tempL: 12,
+          sunrise: "7:25",
+          sunset: "7:25",
+          windSpeed: "14 km/h",
+          humidity: "24%",
+          pressure: "1026.00 mb",
+          uvIndex: 4,
+          cloudCover: "40%",
+          ceiling: "9144m",
+          dewPoint: -6,
+          visibility: 16
+        }
+      ]
+    }
+  }
+];
+
+export default class Forecast extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      weather
+    };
+  }
+
   render() {
     //const { value, children } = this.props;
 
@@ -13,7 +111,11 @@ class Forecast extends Component {
           <Container>
             <Row>
               <Col>
-                <WeatherCard />
+                <WeatherCard
+                  weather={this.state.weather}
+                  Day={this.props.Day}
+                  Tempreture={this.props.tempH}
+                />
                 <WeatherCard />
                 <WeatherCard />
                 <WeatherCard />
@@ -26,5 +128,3 @@ class Forecast extends Component {
     );
   }
 }
-
-export default Forecast;
